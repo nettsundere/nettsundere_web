ee0db2eb439c9a1f5a5d27f17a92dd7d01048458
Vladimir <nettsundere@gmail.com>
2012-12-30 03:42:02 +0400

    Fixed spec

--- spec/models/model_translation_spec.rb
+++ spec/models/model_translation_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "ModelTranslation in ActiveRecord model instance" do
   it "should find translated method" do
-    w = Work.new
+    w = Thing.new
     w.respond_to?(:name).should be_true
     expect { w.name }.to_not raise_error
   end
