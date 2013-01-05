a66f1fd58b872ab1cb7aa0213b22a17714be6799
Vladimir <nettsundere@gmail.com>
2013-01-05 12:28:23 +0400

    Render views!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,9 @@ require 'rspec/autorun'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+
+  config.render_views
+  
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
